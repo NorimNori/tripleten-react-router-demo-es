24fa209fb3180a4f6577051827259be4103e3952
Gloria R <norimaqidoe@hotmail.com>
2026-01-01 23:24:11 -0600

    feat: add routes and links

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,12 +1,12 @@
+import { Link } from "react-router-dom";
 import "./Dashboard.css";
 
 function Dashboard() {
   return (
     <div className="dashboard">
       <h1>El analista de Emojis: todo sobre los Emojis</h1>
-      <p>
-        ¡El destino #1 de opiniones de emojis en la Web desde 2020!
-      </p>
+      <p>¡El destino #1 de opiniones de emojis en la Web desde 2020!</p>
+      <Link to="/reviews">¡Haz clic aquí para ver mis últimas reseñas!</Link>
     </div>
   );
 }
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,25 @@
+import { NavLink } from "react-router-dom";
+import "./NavBar.css";
+
+function NavBar() {
+  const customClassName = ({ isActive }) =>
+    "menu__link" + (isActive ? "menu__link_active" : "");
+  return (
+    <nav className="menu">
+      <NavLink to="/" className={customClassName}>
+        Inicio
+      </NavLink>
+      <NavLink to="/reviews" className={customClassName}>
+        Reseñas de emojis
+      </NavLink>
+      <NavLink to="/about-me" className={customClassName}>
+        Sobre mí
+      </NavLink>
+      <NavLink to="/about-us" className={customClassName}>
+        Sobre nosotros
+      </NavLink>
+    </nav>
+  );
+}
+
+export default NavBar;
