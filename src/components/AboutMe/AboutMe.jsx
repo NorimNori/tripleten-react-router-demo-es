9f433085e0ff751767d4bfeab42e1d969c315db2
Gloria R <norimaqidoe@hotmail.com>
2026-01-01 23:46:12 -0600

    feat: add nested routes

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,6 +1,19 @@
+import { Link } from "react-router-dom";
+
 function AboutMe() {
   return (
     <div>
+      <ul className="links">
+        <li>
+          <Link to="my-story">Mi historia</Link>
+        </li>
+        <li>
+          <Link to="hobbies">Hobbies</Link>
+        </li>
+        <li>
+          <Link to="contact">Mi información de contacto</Link>
+        </li>
+      </ul>
       <p>Soy una persona sencilla. Veo emojis, escribo reseñas.</p>
     </div>
   );
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,9 @@ import Dashboard from "./Dashboard/Dashboard";
 import Reviews from "./Reviews/Reviews";
 import AboutMe from "./AboutMe/AboutMe";
 import AboutUs from "./AboutUs/AboutUs";
+import Contact from "./AboutMe/Contact";
+import Hobbies from "./AboutMe/Hobbies";
+import MyStory from "./AboutMe/MyStory";
 
 function App() {
   return (
@@ -14,7 +17,11 @@ function App() {
         <Route path="/" element={<Dashboard />} />
         <Route path="/reviews" element={<Reviews />} />
         <Route path="/about-me" element={<AboutMe />} />
-        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/about-us" element={<AboutUs />}>
+          <Route path="contact" element={<Contact />} />
+          <Route path="hobbies" element={<Hobbies />} />
+          <Route path="my-story" element={<MyStory />} />
+        </Route>
       </Routes>
     </div>
   );
